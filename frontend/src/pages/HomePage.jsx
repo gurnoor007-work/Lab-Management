6dc3cf36772748a37e6664c320548df12df8af31
gurnoor007-work <work.gurnoor007@gmail.com>
2026-07-11 21:40:38 +0530

    project setup completed

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <div className="flex items-center justify-center bg-amber-400 h-full"><span>Lab Management</span></div>
+    )
+}
