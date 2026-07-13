a2fb60429ad5170cbc52a04397af0f8feb7a8a20
gurnoor007-work <work.gurnoor007@gmail.com>
2026-07-13 17:02:02 +0530

    added hover decorations to navbar

--- frontend/src/components/Navbar/Logo.jsx
+++ frontend/src/components/Navbar/Logo.jsx
@@ -3,8 +3,8 @@ import logo from "../../assets/Logo.png"
 
 export const Logo = () => {
     return (
-        <div className="flex flex-row gap-5 items-center">
-            <img src={logo} alt="Logo" className="h-15 w-15 md:h-21 md:w-21 rounded-2xl" />
+        <div className="flex flex-row gap-5 items-center hover:cursor-pointer hover:scale-101 transition-transform duration-200">
+            <img src={logo} alt="Logo" className="h-15 w-15 md:h-21 md:w-21 rounded-2xl hover:scale-103 transition-transform duration-200"/>
             <p className="text-lg md:text-3xl">Lab Management</p>
         </div>
     )
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,8 +7,9 @@ export const Navbar = () => {
   return (
     <div className="navbar sticky z-50 top-4 mx-auto w-[95%] rounded-3xl bg-[#F3F4F6] shadow-lg overflow-hidden">
       <div className="flex items-center justify-between px-6 py-4">
-        <div className="logo-portion">
+        <div className="logo-portion flex items-center gap-4">
           <Logo />
+          <div className="h-11 w-0.5 bg-[#D1D5DB]"></div>
         </div>
         <div className="user-account">
           {/* <UserDisplay /> */}
